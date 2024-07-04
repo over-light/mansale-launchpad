c680d91a91380a0159dd4fe71024e0af50ba0099
over-light <over.light888@gmail.com>
2024-07-04 17:05:48 +0900

    feat: update appshell, add wallet connect

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -1,37 +1,40 @@
 import { Notifications } from '@mantine/notifications';
 import { ReactNode } from 'react';
 
-import { RootContextProvider } from '@/utils/context';
+import { RootContextProvider } from '@/components/contexts/RootProvider';
 import { ModalsProvider } from '@mantine/modals';
-import { Box, Flex } from '@mantine/core';
+import { AppShell, Box, Burger, Flex, Group, Skeleton } from '@mantine/core';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
+import AppWalletProvider from '../contexts/WalletProvider';
+import { useDisclosure } from '@mantine/hooks';
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const [opened, { toggle }] = useDisclosure();
   return (
     <>
       <Notifications position="top-right" />
       <ModalsProvider>
-        <RootContextProvider>
-          <Flex
-            h={'100vh'}
-            style={{
-              backgroundColor: 'var(--mantine-color-body)',
-            }}
-          >
-            <Sidebar />
-            <Box
-              pos="relative"
-              flex={1}
-              style={{
-                overflowY: 'auto',
-              }}
+        <AppWalletProvider>
+          <RootContextProvider>
+            <AppShell
+              header={{ height: 60 }}
+              navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+              padding="md"
             >
-              <Navbar />
-              {children}
-            </Box>
-          </Flex>
-        </RootContextProvider>
+              <AppShell.Header>
+                <Group h="100%" px="sm">
+                  <Navbar />
+                  <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                </Group>
+              </AppShell.Header>
+              <AppShell.Navbar p="sm" w="250">
+                <Sidebar />
+              </AppShell.Navbar>
+              <AppShell.Main>{children}</AppShell.Main>
+            </AppShell>
+          </RootContextProvider>
+        </AppWalletProvider>
       </ModalsProvider>
     </>
   );
