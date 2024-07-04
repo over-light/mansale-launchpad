e4260e0f100769a17c97bcdba6d4a3eff82fb115
over-light <over.light888@gmail.com>
2024-07-05 01:24:41 +0900

    feat: add features to home page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,10 +1,12 @@
 import { ColorSchemeToggle } from '@/components/index/ColorSchemeToggle/ColorSchemeToggle';
+import Features from '@/components/index/Features';
 import { Welcome } from '@/components/index/Welcome/Welcome';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
+      <Features />
     </>
   );
 }
